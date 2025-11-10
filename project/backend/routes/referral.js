1966c44d18ff011d95a696842c71a22370e6259b
jungjaewon9204-cpu <jungjaewon9204@gmail.com>
2025-11-10 12:40:57 +0300

    Delete project/backend/routes/referral.js

--- project/backend/routes/referral.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// GET /api/referral/code
-router.get('/code', (req, res) => {  
-  res.json({message: 'referral code placeholder'});  
-});
-
-module.exports = router;  
