43d90ecc0a45b7a5c3731547c35d9eb08b1f4945
jungjaewon9204-cpu <jungjaewon9204@gmail.com>
2025-11-10 12:44:01 +0300

    Delete project/backend/models/Admin.js

--- project/backend/models/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  role: { type: String, default: 'admin' },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Admin', AdminSchema);  
