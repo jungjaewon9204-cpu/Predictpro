03c9c0b8a3a77a486caef7a9d475b574a62e558a
jungjaewon9204-cpu <jungjaewon9204@gmail.com>
2025-11-10 12:35:48 +0300

    Delete project/backend/routes/payment.js

--- project/backend/routes/payment.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// POST /api/payment/charge
-router.post('/charge', (req, res) => {  
-  res.json({message: 'payment charge placeholder'});  
-});
-
-module.exports = router;  
